8cf2de6be786112cb7f331e7315155c8bffceab9
daniela2001-png <58857379+daniela2001-png@users.noreply.github.com>
2020-03-30 18:10:20 -0500

    Create Dog Years.js

---
+++ Dog Years.js
@@ -0,0 +1,21 @@
+let myAge = 18; // give a value to my var
+
+let earlyYears = 2;//maybe more later will change
+
+earlyYears = earlyYears * 2;
+
+let laterYears = myAge - 2;
+
+laterYears = laterYears * 4; // multiply  
+
+console.log(earlyYears);
+
+console.log(laterYears);
+
+var myAgeInDogYears = laterYears + earlyYears;
+
+let myName = 'Daniela';
+
+myName = myName.toLowerCase();// convert to lowercase//
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears}years old in dog years.`);
