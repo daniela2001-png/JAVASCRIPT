c83040f7d02fd2b71bd153ba4c1450dad079f26c
daniela2001-png <dmbmora@outlook.com>
2020-05-17 21:03:09 -0500

    loops

---
+++ arrays/do_while.js
@@ -0,0 +1,8 @@
+//do ..while//
+let cupsOfSugarNeeded = 10;
+let cupsAdded = 0;
+do{
+  //bucle que se incremente cupsAddeden uno mientras cupsAddedsea ​​menor que cupsOfSugarNeeded.//
+  cupsAdded++;
+}while(cupsAdded < cupsOfSugarNeeded)
+console.log(cupsOfSugarNeeded)
