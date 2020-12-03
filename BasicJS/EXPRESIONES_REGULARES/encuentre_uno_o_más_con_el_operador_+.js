643a1d581642cedd179096659c2e9256a45598f8
daniela2001-png <dmbmora@outlook.com>
2020-12-03 18:24:31 -0500

    [new] como hacer  coincidir con un cracter una o mas veces con el operador[+] en js

---
+++ BasicJS/EXPRESIONES_REGULARES/encuentre_uno_o_más_con_el_operador_+.js
@@ -0,0 +1,34 @@
+/*
+Expresiones regulares: encuentre uno o más criminales en una cacería
+
+Es hora de hacer una pausa y probar sus nuevas habilidades de escritura de expresiones regulares.
+Un grupo de delincuentes se escapó de la cárcel y se escapó, pero no se sabe cuántos.
+Sin embargo, sabes que permanecen juntos cuando están cerca de otras personas.
+Eres responsable de encontrar a todos los criminales a la vez.
+
+Aquí hay un ejemplo para revisar cómo hacer esto:
+
+La expresión regular /z+/coincide con la letra z cuando aparece una o más veces seguidas.
+Encontraría coincidencias en todas las siguientes cadenas:
+
+"z"
+"zzzzzz"
+"ABCzzzz"
+"zzzzABC"
+"abczzzzzzzzzzzzzzzzzzzzzabc"
+
+Pero no encuentra coincidencias en las siguientes cadenas ya que no hay letras z:
+
+""
+"ABC"
+"abcabc"
+
+Escribe una expresión regular codiciosa que encuentre uno o más criminales
+dentro de un grupo de otras personas. Un criminal está representado por la letra mayúscula C.
+
+*/
+
+let string = "abcabc CC CCC  CCCCCCCC3CC3C3C3C3C";
+let reCriminals = /C+/g;
+let result = string.match(reCriminals);
+console.log(result); // output === ['CC', 'CCC', 'CCCCCCCC', 'CC', 'C', 'C', 'C', 'C']
