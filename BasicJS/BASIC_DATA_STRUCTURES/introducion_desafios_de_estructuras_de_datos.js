2731053f1b0207a08ddfa1a714534f176dad13ac
daniela2001-png <dmbmora@outlook.com>
2020-12-09 20:06:06 -0500

    [new] INTRODUCCION A DESAFIO SDE ESTRUCTURAD DE DATOS EN JS

---
+++ BasicJS/BASIC_DATA_STRUCTURES/introducion_desafios_de_estructuras_de_datos.js
@@ -0,0 +1,12 @@
+/*
+Introducción a los desafíos de la estructura básica de datos:
+
+Los datos se pueden almacenar y acceder de muchas formas diferentes,
+tanto en JavaScript como en otros lenguajes. Esta sección le enseñará cómo manipular matrices,
+así como cómo acceder y copiar la información que contienen.
+También le enseñará cómo manipular y acceder a los datos dentro de los objetos JavaScript,
+utilizando la notación de puntos y corchetes.
+Cuando haya terminado con esta sección, debe comprender las propiedades básicas y las diferencias
+entre matrices y objetos, así como cómo elegir cuál usar para un propósito determinado.
+
+*/
