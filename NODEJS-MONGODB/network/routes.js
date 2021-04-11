fec197c983b67a082ad2c1bb149ccc09fd6834d8
daniela2001-png <dmbmora@outlook.com>
2021-04-11 16:33:59 -0500

    añado el router principal de la app express para suarlo en el server.js

---
+++ NODEJS-MONGODB/network/routes.js
@@ -0,0 +1,8 @@
+const mensaje = require('../components/message/network')
+
+
+const router = (server) => {
+    server.use('/', mensaje) // las rutas funcionan como : '/' + 'nombre ruta del componente' == '/lista-mensajes' => me llamara al endpoint correspondiente
+}
+// agregamos esta funcion de router para que cuando tengamos mas e 1 componente en el server no hagamos o creamos multiples routes , mientras que nuestra API siga creciendo en cuanto al numero de componentes(mensjaes, usuarios)
+module.exports = router
