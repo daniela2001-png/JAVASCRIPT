9ee0774f1e0e1f38f346890a5716457ab16e1318
daniela2001-png <dmbmora@outlook.com>
2021-04-11 16:32:01 -0500

    añado el archivo de configuracion del servidor express

--- NODEJS-MONGODB/server.js
+++ NODEJS-MONGODB/server.js
@@ -1,40 +1,15 @@
 // Aqui crearemos nuestro servidor usando express
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const router = express.Router()
-const response = require('./network/response');
+//const router = require('./components/message/network')
+const router = require('./network/routes')
 const PORT = 3000
 
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true })) // si ponemos el modo extended en false nos traera un objeto con su prototype en cambio con true  se acomoda y crea el objeto simple como se lo mandamos en el request
-
-app.use(router) // hacemos este paso para que el router funcione sin problemas
-
-
-router.get('/message', (req, resp) => {
-    console.log('SOY UN QUERY PARAM EN FORMA DE LISTA: ', req.query.color)
-    // si queremos ver las cabeceras del req usamos el atributo headers del request
-    console.log('HOLA SOY EL HEADER DEL REQUEST: ', req.headers)
-    // si queremos personalizar el header de respuesta del server usamos el metodo header del reponse :)
-    resp.header({
-        'dani-header': 'soy un header personalizado by daniela'
-    })
-
-    response.success(req, resp, 'Lista de mensajes', 200)
-})
-
-router.post('/message-post', (req, resp) => {
-    console.log(req.body)
-    console.log(req.query) // obtenemos el objeto de querys que tengamos(que sena enviados desde el request) en esta ruta 
-    response.success(req, resp, 'mensaje creado correctamente', 201)
-})
-
-router.delete('/message-delete', (req, resp) => {
-    console.log(req.body)
-    req.query.error == "ok" ? response.error(req, resp, 'oops algo ha salido mal', 400) :
-        response.success(req, resp, 'mensaje eliminado correctamente', 204)
-})
+router(app) // creamos los middlewares para las rutas de nuestros componentes
 
 // servimos archivos estaticos con express
 const options = {
@@ -46,7 +21,7 @@ const options = {
     redirect: false,
 }
 // desde la ruta /static/ podemos acceder a las rutas (names directorys) que esten dentro de public por ejemplo: si pongo /static/css/index.css me mostrara el css que he creado :)
-app.use('/static', express.static('public', options)) 
+app.use('/static', express.static('public', options))
 
 
 app.listen(PORT, () => {
