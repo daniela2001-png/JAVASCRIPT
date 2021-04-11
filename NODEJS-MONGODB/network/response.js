df22f5b24f98eb8f6e2f6b503307023999532a0f
daniela2001-png <dmbmora@outlook.com>
2021-04-11 16:29:37 -0500

    añado el archivo de respuestas ante procesos erroneos o exitosos para dar responses sensatos de manera segura

--- NODEJS-MONGODB/network/response.js
+++ NODEJS-MONGODB/network/response.js
@@ -1,4 +1,5 @@
 // este modulo se encargara de exportar dos funciones de respuestas: error y exito
+const chalk = require('chalk');
 
 function success(req, resp, mensaje, status) {
     resp.status(status || 200).send({
@@ -6,7 +7,9 @@ function success(req, resp, mensaje, status) {
         "body": mensaje
     })
 }
-function error(req, resp, mensajeError, status) {
+function error(req, resp, mensajeError, status, detalles) {
+    // usamos la libreria chalk(NO NATIVA) para darle un color rojo a la letra del error
+    console.error(chalk.red('LA CAUSA PRINCIPAL DEL ERROR ES:'), detalles)
     resp.status(status || 400).send({
         "body": null,
         "error": mensajeError
