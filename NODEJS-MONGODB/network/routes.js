7866b468b7aec063abc1f650a4c1995a7e320e41
daniela2001-png <dmbmora@outlook.com>
2021-04-16 21:38:11 -0500

    agregao la ruta del componente chat al archivo de configuracion del servidor

--- NODEJS-MONGODB/network/routes.js
+++ NODEJS-MONGODB/network/routes.js
@@ -1,10 +1,11 @@
 const mensaje = require('../components/message/network')
 const user = require('../components/usuarios/network')
-
+const chat = require('../components/chat/network')
 
 const router = (server) => {
     server.use('/', mensaje) // las rutas funcionan como : '/' + 'nombre ruta del componente' == '/lista-mensajes' => me llamara al endpoint correspondiente
     server.use('/', user)
+    server.use('/', chat)
 }
 // agregamos esta funcion de router para que cuando tengamos mas e 1 componente en el server no hagamos o creamos multiples routes , mientras que nuestra API siga creciendo en cuanto al numero de componentes(mensjaes, usuarios)
 module.exports = router
